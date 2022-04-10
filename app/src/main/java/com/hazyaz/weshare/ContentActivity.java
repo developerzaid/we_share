923029553282286745592a76e556704ebdb0e897
HazyazProjects <85434221+HazyazProjects@users.noreply.github.com>
2022-04-10 16:44:19 +0530

    Front Page Done

--- app/src/main/java/com/hazyaz/weshare/ContentActivity.java
+++ app/src/main/java/com/hazyaz/weshare/ContentActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.widget.Toolbar;
 
+import com.smarteist.autoimageslider.SliderView;
+
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -14,19 +17,14 @@ import static java.security.AccessController.getContext;
 
 public class ContentActivity extends AppCompatActivity {
 
-    int currentPage = 0;
-    Timer timer;
-    final long DELAY_MS = 500;//delay in milliseconds before task is to be executed
-    final long PERIOD_MS = 2000;
 
-    ViewPager viewPager;
+
+    final int[] images ={R.drawable.do1,R.drawable.do2,R.drawable.do3,R.drawable.do4,R.drawable.do5};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
-
-
 
 
     }
